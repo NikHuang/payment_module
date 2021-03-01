ee9104f1d197d255e096573184be9c1a43a91dd5
NikHuang <nikhhq@sina.cn>
2021-03-01 08:33:38 +0800

    修改

--- src/main/java/com/hhq/payment/PaymentApplication.java
+++ src/main/java/com/hhq/payment/PaymentApplication.java
@@ -2,10 +2,11 @@ package com.hhq.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication(scanBasePackages = {"com.hhq"})
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/hhq/payment/controller/TestController.java
+++ src/main/java/com/hhq/payment/controller/TestController.java
@@ -4,7 +4,9 @@ import com.hhq.common_sdk.pom_model.PomModelSingleton;
 import com.hhq.common_sdk.result.Result;
 import com.hhq.payment.domain.Payment;
 import com.hhq.payment.service.IPaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
+@Slf4j
 public class TestController {
 
      @Autowired
     IPaymentService paymentService;
 
+     @Value("${server.port}")
+     private String port;
 
-    @GetMapping("/testGetPayment/{id}")
-    public Result<Payment> getResult(@PathVariable("id")String id){
+
+    @GetMapping(value = "/testGetPayment/{id}",produces = {"application/json;charset=UTF-8"})
+    public Payment getResult(@PathVariable("id")String id){
         Payment payment = paymentService.selectByPrimaryKey(id);
-        return Result.success(payment);
+        log.info(port);
+        return payment;
     }
 }
